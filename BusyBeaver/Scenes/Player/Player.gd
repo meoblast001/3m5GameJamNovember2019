5a3e56bca0925e4c43b19693d23c85d37558dd8e
monsterkrampe <monsterkrampe@users.noreply.github.com>
2019-11-09 19:00:45 +0100

    try out wall jumping

--- BusyBeaver/Scenes/Player/Player.gd
+++ BusyBeaver/Scenes/Player/Player.gd
@@ -4,11 +4,15 @@ extends KinematicBody2D
 export var GRAVITY = 200.0
 export var SPEED = 400 # pixels / sec
 export var JUMP_HEIGHT = 400
-export var IN_AIR_DELAY = 0.1
+export var IN_AIR_DELAY = 0.2
+export var MAX_JUMP_COUNT = 2
 var screen_size
 var jump_counter = 0
 var in_air_timer : float
 var walkAnimationPlaying : bool
+var last_wall = 'none'
+var last_jumped_wall = 'none'
+var is_wall_jump : bool
 
 var velocity = Vector2()
 
@@ -26,20 +30,40 @@ func handleGravity(delta):
 
 func handleJump(delta):
 	in_air_timer += delta
+	
 	if is_on_floor():
 		jump_counter = 0
 		in_air_timer = 0
-	elif in_air_timer > IN_AIR_DELAY:
+		last_jumped_wall = 'none'
+		last_wall = 'none'
+		is_wall_jump = false
+	
+	elif in_air_timer > IN_AIR_DELAY and is_on_wall():
+		var collision = get_slide_collision(0)
+		var current_wall = 'left' if collision.position.x < position.x else 'right'
+		
+		print(current_wall, last_jumped_wall)
+		
+		if current_wall != last_jumped_wall:
+			jump_counter = 0
+			in_air_timer = 0
+		
+		is_wall_jump = true
+		last_wall = current_wall
+	
+	if in_air_timer > IN_AIR_DELAY:
 		jump_counter = max(jump_counter, 1)
 		
 	
-	if jump_counter < 2 and Input.is_action_just_pressed("ui_up"):
-		print(jump_counter)
+	if jump_counter < MAX_JUMP_COUNT and Input.is_action_just_pressed("ui_up"):
 		jump_counter += 1
 		
 		# maybe we should add jump height instead of setting it?
 		velocity.y = -JUMP_HEIGHT
 		if jump_counter == 1:
+			if is_wall_jump:
+				last_jumped_wall = last_wall
+			
 			walkAnimationPlaying = false
 			$AnimationPlayer.play("Jump")
 		else:
