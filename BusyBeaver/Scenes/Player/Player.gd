2cacd63d8c5c693553ba036329902d906a0ac020
monsterkrampe <monsterkrampe@users.noreply.github.com>
2019-11-09 13:29:28 +0100

    put player in level scene

--- BusyBeaver/Scenes/Player/Player.gd
+++ BusyBeaver/Scenes/Player/Player.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 # Declare member variables here. Examples:
 export var max_speed = 400 # pixels / sec
-export var acc = 10
+export var acc = 50
 var screen_size
 var jump_timer : float
 
@@ -16,10 +16,15 @@ func _ready():
 func _integrate_forces(state):
 	jump_timer += state.step
 	
-	if state.linear_velocity.x < max_speed and Input.is_action_pressed("ui_right"):
-		state.apply_central_impulse(Vector2(min(max_speed - state.linear_velocity.x, acc), 0))
-	if state.linear_velocity.x > -max_speed and Input.is_action_pressed("ui_left"):
-		state.apply_central_impulse(Vector2(max(-max_speed - state.linear_velocity.x, -acc), 0))
+	if Input.is_action_pressed("ui_right"):
+		if state.linear_velocity.x < max_speed:
+			state.apply_central_impulse(Vector2(min(max_speed - state.linear_velocity.x, acc), 0))
+	elif Input.is_action_pressed("ui_left"):
+		if state.linear_velocity.x > -max_speed:
+			state.apply_central_impulse(Vector2(max(-max_speed - state.linear_velocity.x, -acc), 0))
+	else:
+		if state.linear_velocity.x != 0:
+			state.apply_central_impulse(Vector2(-state.linear_velocity.x, 0))
 	if jump_timer > 0.5 and Input.is_action_just_pressed("ui_up"):
 		jump_timer = 0
 		state.apply_central_impulse(Vector2(0, -98))
