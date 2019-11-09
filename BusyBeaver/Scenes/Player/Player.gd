b6a7b8204655c99e0fc96cf01e29b80cb05ac58f
monsterkrampe <monsterkrampe@users.noreply.github.com>
2019-11-09 15:23:07 +0100

    prevent player from sliding down slopes

--- BusyBeaver/Scenes/Player/Player.gd
+++ BusyBeaver/Scenes/Player/Player.gd
@@ -6,6 +6,7 @@ export var SPEED = 400 # pixels / sec
 export var JUMP_HEIGHT = 400
 var screen_size
 var jump_timer : float
+var walkAnimationPlaying : bool
 
 var velocity = Vector2()
 
@@ -18,7 +19,10 @@ func _ready():
 
 func _physics_process(delta):
 	jump_timer += delta
-	velocity.y += delta * GRAVITY
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y += delta * GRAVITY
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
@@ -26,26 +30,24 @@ func _physics_process(delta):
 		velocity.x = SPEED
 	else:
 		velocity.x = 0
-	
-#	if Input.is_action_pressed("ui_right"):
-#		if state.linear_velocity.x < max_speed:
-#			state.apply_central_impulse(Vector2(min(max_speed - state.linear_velocity.x, acc), 0))
-#	elif Input.is_action_pressed("ui_left"):
-#		if state.linear_velocity.x > -max_speed:
-#			state.apply_central_impulse(Vector2(max(-max_speed - state.linear_velocity.x, -acc), 0))
-#	else:
-#		if state.linear_velocity.x != 0:
-#			state.apply_central_impulse(Vector2(-state.linear_velocity.x, 0))
+
 	if jump_timer > 0.5 and Input.is_action_just_pressed("ui_up"):
 		jump_timer = 0
+		
+		# maybe we should add jump height instead of setting it?
 		velocity.y = -JUMP_HEIGHT
+		$AnimationPlayer.play("Jump")
 	
 	if velocity.x != 0:
 		$Sprite.flip_h = velocity.x < 0
-		$AnimationPlayer.play("Walk")
+		if not $AnimationPlayer.is_playing():
+			walkAnimationPlaying = true
+			$AnimationPlayer.play("Walk")
 	else:
-		$AnimationPlayer.seek(0, true)
-		$AnimationPlayer.stop()
+		if walkAnimationPlaying:
+			walkAnimationPlaying = false
+			$AnimationPlayer.seek(0, true)
+			$AnimationPlayer.stop()
 		
 	# The second parameter of move_and_slide is the normal pointing up.
 	# In the case of a 2d platformer, in Godot upward is negative y, which translates to -1 as a normal.
