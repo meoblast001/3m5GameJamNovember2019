df74274e09639427215c9e52d3d002a8fa2dc5bb
masterredstorm <steve@gotzel.com>
2019-11-10 17:43:36 +0100

    ADD SFX for switch

--- BusyBeaver/Scenes/Switch/Switch.gd
+++ BusyBeaver/Scenes/Switch/Switch.gd
@@ -7,12 +7,13 @@ var num_activation_attempts: int = 0
 func _on_Area2D_body_entered(body: PhysicsBody2D):
 	if body.is_in_group("torch"):
 		if num_activation_attempts == 0:
+			$"SFX-Toggle".play()
 			emit_signal("activation_changed", true)
 		num_activation_attempts += 1
-
 
 func _on_Area2D_body_exited(body: PhysicsBody2D):
 	if body.is_in_group("torch"):
 		num_activation_attempts -= 1
 		if num_activation_attempts == 0:
+			$"SFX-Toggle".play()
 			emit_signal("activation_changed", false)
